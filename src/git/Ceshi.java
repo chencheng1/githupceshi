2677f4a5839c5f959797fdd8ddbffe134e255e43
cc <1760036402@qq.com>
2017-12-13 19:25:21 +0800

    新增

--- src/git/Ceshi.java
+++ src/git/Ceshi.java
@@ -3,7 +3,9 @@ package git;
 public class Ceshi {
 
 	public static void main(String[] args) {
-		System.out.println("git≥¢ ‘");
+		System.out.println("gitchangshi");
+		System.out.println("gitchangshi");
+		System.out.println("gitchangshi");
 	}
 
 }
